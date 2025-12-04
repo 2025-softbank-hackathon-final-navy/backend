c73c64e9c21256677d0dfd839f82983b5ddcf22a
hanseul37 <hanseul37@daum.net>
2025-12-04 15:34:30 +0900

    [FEAT] MySQL Connection

---
+++ execution-server/database/code.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+host = os.getenv("DB_HOST")
+username = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+database = os.getenv("DB_NAME")
+
+DB_URL = f'mysql+pymysql://{username}:{password}@{host}/{database}'
+
+engine = create_engine(DB_URL)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
